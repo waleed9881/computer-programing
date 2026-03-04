8145085f871bee67327dd22342af209daa5e564d
waleed9881 <mwaleedjaved180@gmail.com>
2026-03-05 00:25:32 +0500

    Update LAB_2 TASK_1.cpp

--- lab 2/lab task/LAB_2 TASK_1.cpp
+++ lab 2/lab task/LAB_2 TASK_1.cpp
@@ -1,7 +1,7 @@
-// Name: Abdul Ahad
-// Department: Electrical Engineering (AI)
-// Registration No: BF25NWELE0692
-// Section: B
+// Name:Muhammad Waleed Javed
+// Department: Electrical Engineering (power)
+// Registration No: BF25NWELE0749
+// Section: A
 
 
 
@@ -14,6 +14,7 @@ int main()
 
     return 0;         //PROGRAM END krny k liye
 }
+
 
 
 
