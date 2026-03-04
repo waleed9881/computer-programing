3fc236fc5f8b213ffa4c11e97ac827dc289904c3
waleed9881 <mwaleedjaved180@gmail.com>
2026-03-04 23:48:09 +0500

    Add files via upload

---
+++ Lab 1/lab work/LAB WORK (LAB 1).cpp
@@ -0,0 +1,17 @@
+// Name: Abdul Ahad
+// Department: Electrical Engineering (AI)
+// Registration No: BF25NWELE0692
+// Section: B
+
+
+
+#include <iostream>   // library ha iostream ma 
+using namespace std;  // standard name use krny k liay add kiay han
+
+int main() 
+{
+    
+    cout << "Hello World!"; // display k liye
+
+    return 0;  // program shuru b aur khatam b krta ha
+}
---
+++ Lab 1/lab work/My program lab  1.cpp
@@ -0,0 +1,32 @@
+// Name: Abdul Ahad
+// Department: Electrical Engineering (AI)
+// Registration No: BF25NWELE0692
+// Section: B
+
+
+#include <iostream>
+#include<string>
+using namespace std;
+int main(){
+	
+int n = 6; //INIITIALIZATION K LIYE
+
+float o = 34.98797; //POINT VALUES K LYE USE KRTY HAN
+
+string asdf = "oghi";   //CHAR KI TRAH KAM KRTA HA 
+
+char p = 'g';  // CHARCTER PRINT KRNY K LIYE
+
+cout<<n<<endl<<o<<endl<<asdf<<endl<<p<<endl;	
+	int i=123
+	for (i=0 ; i<100 ; i++);  //FOR CONDITION DENY K LIYE
+	
+	cout<<a; // DISPLAY K LIYE
+	
+	
+	
+
+
+
+
+}
