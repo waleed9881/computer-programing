1a243902f9752b06f20217efcc0c3fd2781701e6
waleed9881 <mwaleedjaved180@gmail.com>
2026-03-05 00:30:31 +0500

    Update and rename LAB 2 TASK 2.cpp to LAB 2 lab work 2.cpp

--- lab 2/lab work/LAB 2 lab work 2.cpp
+++ lab 2/lab work/LAB 2 lab work 2.cpp
@@ -1,7 +1,7 @@
-// Name: Abdul Ahad
-// Department: Electrical Engineering (AI)
-// Registration No: BF25NWELE0692
-// Section: B
+// Name:Muhammad Waleed Javed
+// Department: Electrical Engineering (power)
+// Registration No: BF25NWELE0749
+// Section: A
 
 
 #include <iostream>
@@ -15,3 +15,4 @@ int main()
 
 return 0;
 }
+
