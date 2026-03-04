34547b3d9e70cca49de022612bf32248ffb4b919
waleed9881 <mwaleedjaved180@gmail.com>
2026-03-05 00:14:03 +0500

    Update LAB 1 TASK 10.cpp

--- Lab 1/lab task/LAB 1 TASK 10.cpp
+++ Lab 1/lab task/LAB 1 TASK 10.cpp
@@ -1,8 +1,7 @@
-// Name: Abdul Ahad
-// Department: Electrical Engineering (AI)
-// Registration No: BF25NWELE0692
-// Section: B
-
+// Name:Muhammad Waleed Javed
+// Department: Electrical Engineering (power)
+// Registration No: BF25NWELE0749
+// Section: A
 
 #include <iostream>
 using namespace std;
@@ -19,4 +18,5 @@ int main()
     
     return 0;   // indicate that program ended successfully
     } // end function main
+
 
