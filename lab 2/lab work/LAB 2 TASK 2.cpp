600e3a505b7ea54ec01d81b5589e3321e11749a5
waleed9881 <mwaleedjaved180@gmail.com>
2026-03-04 23:51:49 +0500

    Add files via upload

---
+++ lab 2/lab work/LAB 2 TASK 2.cpp
@@ -0,0 +1,17 @@
+// Name: Abdul Ahad
+// Department: Electrical Engineering (AI)
+// Registration No: BF25NWELE0692
+// Section: B
+
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+    	float pi; // FLAOT USE KIA HA DECIMAL VALUE K LIYE
+    	pi=3.14; // VALUE DECLARE
+    	cout<<"value of pi =\t"<<pi; // DISPLAY
+
+
+return 0;
+}
---
+++ lab 2/lab work/LAB 2 TASK 3.cpp
@@ -0,0 +1,22 @@
+// Name: Abdul Ahad
+// Department: Electrical Engineering (AI)
+// Registration No: BF25NWELE0692
+// Section: B
+
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+    	double F; // WORDS LIKHNY K LIYE
+    	float C; // DECIMAL VALUE K LIYE 
+    	cout<<"enter temperature in celcius ";  // DISPLAY
+    	cin>>C;
+    F = (C * 9/5) + 32;  // FORMULA CONVERRSION K LIYE
+    	cout<<F <<"  farheight";
+	
+
+return 0;
+}
