e7436dee314abf37137b0c69175c0401cb787763
waleed9881 <mwaleedjaved180@gmail.com>
2026-05-19 02:06:41 +0500

    Add files via upload

---
+++ lab 9/lab work/Lab_09 LabWork_01.cpp
@@ -0,0 +1,22 @@
+// Name: JABRAN KHAN
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0746
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void greet()
+{
+    // Display greeting message
+    cout << "Welcome to C++ Programming!" << endl;
+}
+
+int main()
+{
+    // Function call
+    greet();
+
+    return 0;
+}
---
+++ lab 9/lab work/Lab_09 LabWork_02.cpp
@@ -0,0 +1,22 @@
+// Name: JABRAN KHAN
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0746
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void sayHello()
+{
+    // Display message
+    cout << "Hello, World!" << endl;
+}
+
+int main()
+{
+    // Function call
+    sayHello();
+
+    return 0;
+}
