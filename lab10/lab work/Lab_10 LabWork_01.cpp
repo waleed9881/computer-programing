ffda1ad30facb753d3bc5b5523317b5fa35f3634
waleed9881 <mwaleedjaved180@gmail.com>
2026-05-19 02:07:06 +0500

    Add files via upload

---
+++ lab10/lab work/Lab_10 LabWork_01.cpp
@@ -0,0 +1,25 @@
+// Name: JABRAN KHAN
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0746
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+// Function with parameters
+void multiply(int num1, int num2)
+{
+    // Calculate product
+    int product = num1 * num2;
+
+    // Display result
+    cout << "Product = " << product << endl;
+}
+
+int main()
+{
+    // Function call with two numbers
+    multiply(5, 4);
+
+    return 0;
+}
