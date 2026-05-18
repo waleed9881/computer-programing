7fd2b7e2cebd82063e21962ac7fb64e44dd142ae
waleed9881 <mwaleedjaved180@gmail.com>
2026-05-19 02:04:31 +0500

    Add files via upload

---
+++ lab 6/lab work/LAB_6 LabWork_1.cpp
@@ -0,0 +1,18 @@
+// Name: WALEED JAVED
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0749
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;   // starting point
+
+    while (i <= 10) {
+        cout << i << endl;  // print number on new line
+        i++;                // move to next number
+    }
+
+    return 0;
+}
